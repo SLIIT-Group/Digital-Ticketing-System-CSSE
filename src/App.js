1eb3f0ec8a98f9bc36a7f1c64705cd0677a101b1
imalK96 <imalkrulez@gmail.com>
2020-10-15 10:50:51 +0530

    Moved useEffect in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
-import {Provider} from 'react-redux';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from './store';
-import {loadUser} from "./actions/pasActions";
+import { loadUser } from './actions/pasActions';
 import Login from './components/User/Login';
 import Register from './components/User/Register';
 import UserHome from './components/User/UserHome';
@@ -15,25 +10,23 @@ import ManagerHome from './components/Manager/ManagerHome';
 import InspectorHome from './components/Inspector/InspectorHome';
 import disableBrowserBackButton from 'disable-browser-back-navigation';
 
-
-useEffect(() => {
-  store.dispatch(loadUser());
-  disableBrowserBackButton();
-});
-
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+    disableBrowserBackButton();
+  });
   return (
     <Router>
       <Provider store={store}>
-      <Switch>
-        <div className='App'>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/user' component={UserHome}></Route>
-          <Route exact path='/manager' component={ManagerHome}></Route>
-          <Route exact path='/inspector' component={InspectorHome}></Route>
-          <Route exact path='/' component={Login} />
-        </div>
-      </Switch>
+        <Switch>
+          <div className='App'>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/user' component={UserHome}></Route>
+            <Route exact path='/manager' component={ManagerHome}></Route>
+            <Route exact path='/inspector' component={InspectorHome}></Route>
+            <Route exact path='/' component={Login} />
+          </div>
+        </Switch>
       </Provider>
     </Router>
   );
