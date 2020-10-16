9c2fbfa7aa8dfce9264952b8937a1f17e41e37a0
imalK96 <imalkrulez@gmail.com>
2020-10-16 12:03:45 +0530

    Added account details

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ViewTokens from './components/Inspector/ViewTokens';
 import PassengerDetails from './components/Inspector/PassengerDetails';
 import TokenForm from './components/Inspector/TokenForm';
 import GenerateTImeTable from './components/Manager/GenerateTImeTable';
+import AccountDetails from './components/User/AccountDetails';
 
 function App() {
   useEffect(() => {
@@ -29,6 +30,11 @@ function App() {
           <div className='App'>
             <Route exact path='/register' component={Register} />
             <Route exact path='/user' component={UserHome}></Route>
+            <Route
+              exact
+              path='/user/account'
+              component={AccountDetails}
+            ></Route>
             <Route exact path='/manager' component={ManagerHome}></Route>
             <Route exact path='/inspector' component={InspectorHome}></Route>
             <Route
