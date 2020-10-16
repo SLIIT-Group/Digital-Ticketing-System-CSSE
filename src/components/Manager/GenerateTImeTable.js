5bdcc025aecf25646684fc18cbd8155f3a3de25f
Heshan <hrathnapala@gmail.com>
2020-10-16 07:56:24 +0530

    TimeTable Form

--- src/components/Manager/GenerateTImeTable.js
+++ src/components/Manager/GenerateTImeTable.js
@@ -15,30 +15,66 @@ import {
   FormGroup,
   Label,
   Input,
-  Navbar,
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 
-const paymentMethod = [
-  {
-    value: "Credit Card",
-    label: "Credit Card",
-  },
-  {
-    value: "Debit card",
-    label: "Debit card",
-  },
-  {
-    value: "Frimi Account",
-    label: "Frimi Account",
-  },
+const locations = [
+  "Colombo",
+  "kandy",
+  "Galle",
+  "Ampara",
+  "Anuradhapura",
+  "Badulla",
+  "Batticaloa",
+  "Gampaha",
+  "Hambantota",
+  "Jaffna",
+  "Kalutara",
+  "Kilinochchi",
+  "Kurunegala",
+  "Mannar",
+  "Matale",
+  "Matara",
+  "Monaragala",
+  "Mullativu",
+  "Nuwara Eliya",
+  "Polonnaruwa",
+  "Puttalam",
+  "Ratnapura",
+  "Trincomalee",
+  "Vavuniya",
 ];
 
 function GenerateTImeTable() {
+  const [startLocation, setStartLocation] = useState("");
+  const [endLocation, setEndLocation] = useState("");
+  const [routeNumber, setRouteNumber] = useState("");
+  const [busNumber, setBusNumber] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [distance, setDistance] = useState("");
+  const [price, setPrice] = useState("");
+
+  const filteredLocation = locations.filter(
+    (location_filter) => location_filter !== startLocation
+  );
+
+  const test = (e) => {
+    e.preventDefault();
+    console.log(startLocation);
+    console.log(endLocation);
+    console.log(routeNumber);
+    console.log(busNumber);
+    console.log(startTime);
+    console.log(endTime);
+    console.log(distance);
+    console.log(price);
+  };
+
   return (
-    <div>
+    <React.Fragment>
       <NavBar></NavBar>
       <Container>
         <Row style={{ marginTop: "4em", marginBottom: "4em" }}>
@@ -59,56 +95,99 @@ function GenerateTImeTable() {
                   <Form>
                     <FormGroup>
                       <Label>Route Number</Label>
-                      <Input type="text" />
+                      <Input
+                        type="text"
+                        value={routeNumber}
+                        onChange={(e) => setRouteNumber(e.target.value)}
+                      />
                     </FormGroup>
                     <FormGroup>
                       <Label>Bus Number</Label>
-                      <Input type="text" />
-                    </FormGroup>
-                    <FormGroup>
-                      <Label>Starting From</Label>
-                      <Input type="text" />
-                    </FormGroup>
-                    <FormGroup>
-                      <Label>Going To</Label>
-                      <Input type="text" />
-                    </FormGroup>
-                    <FormGroup>
-                      <Label>Starting Time</Label>
-                      <Input type="time" />
-                    </FormGroup>
-                    <FormGroup>
-                      <Label>Arrival Time</Label>
-                      <Input type="time" />
-                    </FormGroup>
-                    <FormGroup>
-                      <Label>Total Distance</Label>
-                      <Input type="number" />
-                    </FormGroup>
-                    <FormGroup>
-                      <Label>Price Per Km</Label>
-                      <Input type="number" />
+                      <Input
+                        type="text"
+                        value={busNumber}
+                        onChange={(e) => setBusNumber(e.target.value)}
+                      />
                     </FormGroup>
                     <FormGroup>
                       <Label>Starting From</Label>
                       <TextField
                         select
                         label="Starting From"
-                        // value={faculty}
-                        // onChange={handleFacultyChange}
+                        value={startLocation}
+                        onChange={(e) => {
+                          setStartLocation(e.target.value);
+                        }}
                         variant="outlined"
                         fullWidth
                       >
-                        {paymentMethod.map((option) => (
-                          <MenuItem key={option.value} value={option.value}>
-                            {option.value}
+                        {locations.map((location) => (
+                          <MenuItem key={location} value={location}>
+                            {location}
                           </MenuItem>
                         ))}
                       </TextField>
                     </FormGroup>
+                    <FormGroup>
+                      <Label>Going To</Label>
+                      <TextField
+                        select
+                        label="Going To"
+                        disabled={!startLocation}
+                        value={endLocation}
+                        onChange={(e) => {
+                          setEndLocation(e.target.value);
+                        }}
+                        variant="outlined"
+                        fullWidth
+                      >
+                        {filteredLocation.map((location) => (
+                          <MenuItem key={location} value={location}>
+                            {location}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Starting Time</Label>
+                      <Input
+                        type="time"
+                        value={startTime}
+                        onChange={(e) => setStartTime(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Arrival Time</Label>
+                      <Input
+                        type="time"
+                        value={endTime}
+                        onChange={(e) => setEndTime(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Total Distance</Label>
+                      <Input
+                        type="number"
+                        value={distance}
+                        onChange={(e) => setDistance(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Price Per Km</Label>
+                      <Input
+                        type="number"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                      />
+                    </FormGroup>
+
                     <div style={{ display: "flex", justifyContent: "center" }}>
                       <NavLink to="/user">
-                        <Button style={{ backgroundColor: "#f0ad4e" }}>
+                        <Button
+                          type="submit"
+                          style={{ backgroundColor: "#f0ad4e" }}
+                          onClick={(e) => test(e)}
+                        >
                           Save Details
                         </Button>
                       </NavLink>
@@ -125,18 +204,13 @@ function GenerateTImeTable() {
                   textAlign: "center",
                 }}
               >
-                <p>
-                  {/*Not a member?{' '}*/}
-                  <NavLink style={{ color: "#f2f2f2" }} to="/register">
-                    All rights reserved
-                  </NavLink>
-                </p>
+                <p style={{marginTop:10}}>All rights reserved</p>
               </CardFooter>
             </Card>
           </Col>
         </Row>
       </Container>
-    </div>
+    </React.Fragment>
   );
 }
 
